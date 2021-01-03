e96d39af1f0176e0a57cd6a4b0800158e8f9c194
wuxianqiang <2631640352@qq.com>
2021-01-03 16:37:59 +0800

    弹窗动画

--- src/store/mutations/count.js
+++ src/store/mutations/count.js
@@ -8,6 +8,13 @@ const mutation = {
     if (current && target) {
       current.count++
       current.spuList.splice(index, 1, { ...target, count: target.count + 1 })
+      const selectIndex = state.selectList.findIndex(item => item.spuId === spuId)
+      const selectTarget = state.selectList[selectIndex]
+      if (selectTarget) {
+        selectTarget.count++
+      } else {
+        state.selectList.push({ ...target, count: target.count + 1, tag })
+      }
     } else {
       console.log('系统错误')
     }
@@ -20,8 +27,39 @@ const mutation = {
     if (current && target) {
       current.count--
       current.spuList.splice(index, 1, { ...target, count: target.count - 1 })
+      const selectIndex = state.selectList.findIndex(item => item.spuId === spuId)
+      const selectTarget = state.selectList[selectIndex]
+      if (selectTarget) {
+        if (selectTarget.count === 1) {
+          state.selectList.splice(selectIndex, 1)
+          // 关闭弹窗
+          if (state.selectList.length === 0) {
+            state.showVisible = false
+          }
+        } else {
+          selectTarget.count--
+        }
+      }
     } else {
       console.log('系统错误')
+    }
+  },
+  clearCallback (state) {
+    state.showVisible = false
+    state.selectList = []
+    const foodList = state.foodList
+    foodList.forEach(item => {
+      item.count = 0
+      item.spuList.forEach(item => {
+        item.count = 0
+      })
+    })
+  },
+  setState (state, params) {
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        state[key] = params[key]
+      }
     }
   }
 }
--- src/store/state/common.js
+++ src/store/state/common.js
@@ -1,7 +1,9 @@
 // 每个页面的state通过文件进行隔离，防止以后数据很多
 const state = {
   foodList: [],
-  shopInfo: []
+  shopInfo: [],
+  showVisible: false,
+  selectList: []
 }
 
 export default state
